f40770046ec09273f9c6dc303d0c1a9c055061ce
Rich Abdill <richard.abdill@gmail.com>
2018-09-06 23:28:10 -0500

    Filtering by day of week

--- builder.js
+++ builder.js
@@ -1,34 +1,56 @@
-table = document.getElementById("results");
-cols = ["catalog", "title", "semesters", "credits", "type", "time"];
+function build_page() {
+  refresh = document.createElement('tbody');
+  refresh.setAttribute("id", "refresh")
 
-for(var i=0, entry; entry=data[i]; i++) { // each class
-  console.log(entry["catalog"])
-  for(var j=0, section; section=entry["sections"][j]; j++) { // each section
-    row = table.insertRow(-1);
-    for(var k=0, col; col=cols[k]; k++) {
-      cell = row.insertCell(k);
-      if(col == "time") {
-        contents = "";
-        console.log(section);
-        for(var m=0, time; time=section["times"][m]; m++) { // each meeting time for a section
-          for(var n=0, day; day=time["days"][n]; n++) { // each day that it meets at that time
-            contents += day;
-            if(n < time["days"].length - 1) {
-              contents += "/";
-            }
-          }
-          contents += ": " + time["start"] + "&ndash;" + time["end"]
-          if(m < section["times"].length - 1) {
-            contents += " and ";
-          }
-        }
-      } else if(col == "type") {
-        contents = section["type"];
-      } else {
-        contents = entry[col];
-      }
-      cell.innerHTML = contents;
+  cols = ["catalog", "title", "semesters", "credits", "type", "time"];
+  var checkboxes = document.getElementsByName('exclude_day');
+  var exclude_days = [];
+  for (var day of checkboxes) {
+    if (day.checked) {
+      exclude_days.push(day.value)
     }
   }
+
+  for(var entry of data) { // each class
+    for(var section of entry["sections"]) { // each section
+      row = refresh.insertRow(-1);
+      throw_out = false;
+      for(var i=0, col; col=cols[i]; i++) {
+        cell = row.insertCell(i);
+        if(col == "time") {
+          contents = "";
+          for(var k=0, time; time=section["times"][k]; k++) { // each meeting time for a section
+            for(var j=0, day; day=time["days"][j]; j++) { // each day that it meets at that time
+              if(exclude_days.includes(day)) {
+                throw_out = true;
+                break;
+              }
+              contents += day;
+              if(j < time["days"].length - 1) {
+                contents += "/";
+              }
+            }
+            contents += ": " + time["start"] + "&ndash;" + time["end"]
+            if(k < section["times"].length - 1) {
+              contents += " and ";
+            }
+          }
+        } else if(col == "type") {
+          contents = section["type"];
+        } else {
+          contents = entry[col];
+        }
+        cell.innerHTML = contents;
+      }
+      if(throw_out) {
+        row = refresh.deleteRow(refresh.rows.length - 1);
+      }
+    }
+  }
+  table = document.getElementById("resultsTable");
+  old = document.getElementById("results");
+  table.replaceChild(refresh, document.getElementById("results"));
+  refresh.setAttribute("id", "results"); // so we can find it next time
 }
-// {"catalog": "AGRO 5021", "title": "Plant Breeding Principles", "semesters": "Every Fall", "credits": "3", "sections": [{"type": "LEC", "times": [{"start": "10:15", "end": "11:30", "location": "Borlaug Hall 306", "days": ["t", "th"]}]}]}
+
+build_page();
